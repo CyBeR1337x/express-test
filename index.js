12c46d6785eae9806478c4f2f5d4d93119dfc04e
Syed Shah Meer Haider <60551634+CyBeR1337x@users.noreply.github.com>
2025-04-22 00:35:16 +0500

    .

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
 const PORT = process.env.PORT;
 
 app.use(cors());
+app.use(morgan("dev"))
 
 app.get("/", (req, res) => res.json({ sucess: true }));
 
